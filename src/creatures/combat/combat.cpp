010580888d66c6e5d2bd2e798a9b2826146e46f9
Beats-Dh <daniel15042014@hotmail.com>
2021-11-26 22:55:37 -0400

    Fix spells addevent crash after reload
    https: //github.com/opentibiabr/optimized_forgottenserver/commit/63f8930449b6f0c0c5d4ddbc21f9d4d2436a7f17
    Co-Authored-By: Jakub <38609240+SaiyansKing@users.noreply.github.com>

--- src/creatures/combat/combat.cpp
+++ src/creatures/combat/combat.cpp
@@ -85,7 +85,7 @@ CombatDamage Combat::getCombatDamage(Creature* creature, Creature* target) const
 	return damage;
 }
 
-void Combat::getCombatArea(const Position& centerPos, const Position& targetPos, const AreaCombat* area, std::forward_list<Tile*>& list)
+void Combat::getCombatArea(const Position& centerPos, const Position& targetPos, const AreaCombat* area, std::vector<Tile*>& list)
 {
 	if (targetPos.z >= MAP_MAX_LAYERS) {
 		return;
@@ -99,7 +99,7 @@ void Combat::getCombatArea(const Position& centerPos, const Position& targetPos,
 			tile = new StaticTile(targetPos.x, targetPos.y, targetPos.z);
 			g_game.map.setTile(targetPos, tile);
 		}
-		list.push_front(tile);
+		list.push_back(tile);
 	}
 }
 
@@ -779,7 +779,7 @@ void Combat::doCombat(Creature* caster, const Position& position) const
 
 void Combat::CombatFunc(Creature* caster, const Position& pos, const AreaCombat* area, const CombatParams& params, CombatFunction func, CombatDamage* data)
 {
-	std::forward_list<Tile*> tileList;
+	std::vector<Tile*> tileList;
 
 	if (caster) {
 		getCombatArea(caster->getPosition(), pos, area, tileList);
@@ -1292,7 +1292,7 @@ AreaCombat::AreaCombat(const AreaCombat& rhs)
 	}
 }
 
-void AreaCombat::getList(const Position& centerPos, const Position& targetPos, std::forward_list<Tile*>& list) const
+void AreaCombat::getList(const Position& centerPos, const Position& targetPos, std::vector<Tile*>& list) const
 {
 	const MatrixArea* area = getArea(centerPos, targetPos);
 	if (!area) {
@@ -1313,7 +1313,7 @@ void AreaCombat::getList(const Position& centerPos, const Position& targetPos, s
 						tile = new StaticTile(tmpPos.x, tmpPos.y, tmpPos.z);
 						g_game.map.setTile(tmpPos, tile);
 					}
-					list.push_front(tile);
+					list.push_back(tile);
 				}
 			}
 			tmpPos.x++;
