bae33aea14cda8bba0537ea0aefb834975cd5fdc
Beats-Dh <daniel15042014@hotmail.com>
2021-11-27 00:42:37 -0400

    Optimize Player::invitePartyList
    Optimize Player::invitePartyList by switching to std::vector to reduce cache misses and allocations
     master
    
    https: //github.com/opentibiabr/optimized_forgottenserver/commit/642c6de99fe849f922382211feb410163cd1602e
    Co-Authored-By: Jakub <38609240+SaiyansKing@users.noreply.github.com>

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -4905,13 +4905,16 @@ bool Player::addPartyInvitation(Party* newParty)
 		return false;
 	}
 
-	invitePartyList.push_front(newParty);
+	invitePartyList.push_back(newParty);
 	return true;
 }
 
 void Player::removePartyInvitation(Party* remParty)
 {
-	invitePartyList.remove(remParty);
+	auto it = std::find(invitePartyList.begin(), invitePartyList.end(), remParty);
+	if (it != invitePartyList.end()) {
+		invitePartyList.erase(it);
+	}
 }
 
 void Player::clearPartyInvitations()
